c648ef5b110aebc5db968acc10dadb2821a02050
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2023-11-08 02:48:06 +0100

    redis and railway prod

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -9,11 +9,27 @@ const redisClient = redis.createClient({
   /* username: process.env.REDISUSER, */
   password: redisPassword,
   socket: {
-      host: redisHost,
-      port: redisPort,
-      /* tls: true, */
+    host: redisHost,
+    port: redisPort,
+    /* tls: true, */
   }
 });
+
+// Manejar la conexión exitosa
+redisClient.on('connect', () => {
+  console.log('Conexión a Redis exitosa');
+  // Aquí puedes realizar operaciones en la base de datos Redis
+  // No recomendado: Borrar una clave en Redis inmediatamente después de la conexión
+  /* const keyToDelete = 'characters'; // Reemplaza con la clave que deseas eliminar
+  redisClient.del(keyToDelete, (err, reply) => {
+    if (err) {
+      console.error('Error al borrar la clave:', err);
+    } else {
+      console.log(`Clave ${keyToDelete} borrada con éxito.`);
+    }
+  }); */
+});
+
 
 // Manejar errores de conexión
 redisClient.on('error', (err) => {
@@ -21,10 +37,9 @@ redisClient.on('error', (err) => {
   // Puedes agregar lógica adicional aquí, como intentar reconectar.
 });
 
-// Manejar la conexión exitosa
-redisClient.on('connect', () => {
-  console.log('Conexión a Redis exitosa');
-  // Aquí puedes realizar operaciones en la base de datos Redis
+// Evitar errores de cierre inesperados
+redisClient.on('end', () => {
+  console.log('Conexión a Redis cerrada inesperadamente');
 });
 
 export default redisClient;
--- src/v1/routes/postsRoutes.js
+++ src/v1/routes/postsRoutes.js
@@ -15,33 +15,49 @@ router.get('/posts/:postId', postsController.getPostById);
 
 
 // TEST ROUTE TO FETCH DATA FROM URL NOT DATABASE DIRECTLY
-router.get('/cache', async (req, res) => {
 
-  console.log('HOLA???');
+// Función para obtener datos de la API externa
+async function fetchDataFromExternalAPI() {
   try {
-    // Attempt to retrieve data from Redis
+    const response = await axios.get("https://rickandmortyapi.com/api/character");
+    return response.data;
+  } catch (axiosError) {
+    throw new Error('Error en la llamada a la API externa');
+  }
+}
+
+// Función para obtener datos de la caché o la API externa
+async function getDataFromCacheOrAPI() {
+  try {
+    // Intenta recuperar datos de Redis
     const cachedData = await redisClient.get('characters');
+
     if (cachedData) {
-      // Data is cached in Redis, return it
-      res.json(JSON.parse(cachedData));
+      console.log('Datos en caché', cachedData);
+      return JSON.parse(cachedData);
     } else {
-      // Data is not in cache, fetch it from the source
-      const response = await axios.get("https://rickandmortyapi.com/api/character");
-      // Store the response data in Redis for future requests
-      redisClient.set('characters', JSON.stringify(response.data), (err, reply) => {
-        if (err) {
-          console.error(err);
-        }
-        // Return the fetched data to the client
-        res.json(response.data);
-      });
+      // Los datos no están en caché, obténlos de la fuente externa
+      const responseData = await fetchDataFromExternalAPI();
+      // Almacena los datos de respuesta en Redis para futuras solicitudes
+      await redisClient.set('characters', JSON.stringify(responseData));
+      console.log('Datos de respuesta externa', responseData);
+      return responseData;
     }
   } catch (error) {
-    // Handle errors
-    console.error('Error in route handler:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    throw new Error('Error en la obtención de datos');
+  }
+}
+
+router.get('/cache', async (req, res) => {
+  try {
+    const data = await getDataFromCacheOrAPI();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error en la ruta:', error.message);
+    res.status(500).json({ error: 'Error interno del servidor' });
   }
 });
+
 
 
 export { router };
